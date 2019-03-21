9930073d834e913855a2a60d1c5927f76d867cf3
HazDuck <peter.e.heyes@gmail.com>
2019-03-21 14:53:01 +0000

    Login page

---
+++ signin.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+
+<body>
+<form action="admin.php" method="POST">
+    Username<input type="text" name="userName">
+    Password<input type="text" name="password">
+    <input type="submit" name="signInSub" value="Submit">
+</form>
+</body>
